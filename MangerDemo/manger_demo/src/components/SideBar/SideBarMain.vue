a40089262fbc71e04905593b3ed4c773b29a3a55
TangFenQi <18328597432@163.com>
2020-03-13 00:19:07 +0800

    1

--- MangerDemo/manger_demo/src/components/SideBar/SideBarMain.vue
+++ MangerDemo/manger_demo/src/components/SideBar/SideBarMain.vue
@@ -2,46 +2,19 @@
   <div class="sidebar-box">
     <el-scrollbar wrap-class="scroll-bar-wrapper">
       <!--collapse-transaction 是否开启折叠动画。没太大用-->
+      <!--主体样式-->
       <el-menu
         class="el-menu-vertical-demo"
         @open="handleOpen"
-        :background-color="variables.menuBg" 
+        :background-color="variables.menuBg"
         :active-text-color="variables.menuActiveText"
         :text-color="variables.menuText"
         @close="handleClose"
         :collapse="isCollapse"
         :collapse-transaction="false"
       >
-        <el-submenu index="1">
-          <template slot="title">
-            <i class="el-icon-location"></i>
-            <span slot="title">导航一</span>
-          </template>
-          <el-menu-item-group>
-            <span slot="title">分组一</span>
-            <el-menu-item index="1-1">选项1</el-menu-item>
-            <el-menu-item index="1-2">选项2</el-menu-item>
-          </el-menu-item-group>
-          <el-menu-item-group title="分组2">
-            <el-menu-item index="1-3">选项3</el-menu-item>
-          </el-menu-item-group>
-          <el-submenu index="1-4">
-            <span slot="title">选项4</span>
-            <el-menu-item index="1-4-1">选项1</el-menu-item>
-          </el-submenu>
-        </el-submenu>
-        <el-menu-item index="2">
-          <i class="el-icon-menu"></i>
-          <span slot="title">导航二</span>
-        </el-menu-item>
-        <el-menu-item index="3" disabled>
-          <i class="el-icon-document"></i>
-          <span slot="title">导航三</span>
-        </el-menu-item>
-        <el-menu-item index="4">
-          <i class="el-icon-setting"></i>
-          <span slot="title">导航四</span>
-        </el-menu-item>
+        <!--子项目-->
+        <sideBarItem></sideBarItem>
       </el-menu>
     </el-scrollbar>
   </div>
@@ -49,10 +22,14 @@
 
 <script>
 import variables from "@/style/variables.scss";
+import sideBarItem from "./SideBarItem";
 
 //属性可以通过 computed暴露出来。然后在template中使用。
 export default {
   name: "SideBarMain",
+  components: {
+    sideBarItem
+  },
   computed: {
     variables() {
       return variables;
