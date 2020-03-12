fd632fe1cbfb1171500f089fcacb1041a92cdc1a
tangwenbiao <tangwenbiao@sm.vvip-u.com>
2020-03-12 18:14:45 +0800

    登录解决完毕。开始解决侧边栏问题

---
+++ MangerDemo/manger_demo/src/layout/index.vue
@@ -0,0 +1,35 @@
+<template>
+  <div class="layout">
+    <SideBarMain></SideBarMain>
+    <div>
+      <div class="top-bar">
+        <el-button type="danger" round class="log-out" @click="logout">退出</el-button>
+      </div>
+      <div>
+        <router-view></router-view>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import SideBarMain from "@/components/SideBar/SideBarMain";
+import { toLogout } from "@/api/login.js";
+export default {
+  name: "Layout",
+  components: { SideBarMain },
+  methods: {
+    logout: function() {
+      toLogout(this.$router);
+    }
+  }
+};
+</script>
+
+<style lang="scss" scoped>
+.layout {
+  height: 100%;
+  width: 100%;
+  position: relative;
+}
+</style>
--- MangerDemo/manger_demo/src/views/HomeView.vue
+++ MangerDemo/manger_demo/src/views/HomeView.vue
@@ -1,27 +1,15 @@
 <template>
   <div>
-    <div class="top-bar">
-      <el-button type="danger" round class="log-out" @click="logout">退出</el-button>
-    </div>
-    <div>
-      <p>hi,this is home!</p>
-      <router-link to="/order">to order</router-link>
-      <router-link to="/graph">to graph</router-link>
-      <router-view></router-view>
-    </div>
+    <Layout></Layout>
   </div>
 </template>
 
 
 <script>
-import {toLogout} from '@/api/login.js'
+import Layout from "@/layout";
 export default {
   name: "HomeView",
-  methods:{
-      logout:function(){
-        toLogout(this.$router)
-      }
-  },
+  components: { Layout },
   mounted() {
     console.log("查看路由信息");
     console.log(this.$router);
